f9ac2660678afe109f32cc1dcaeec56a0768a412
desolateslacker <91762164+desolateslacker@users.noreply.github.com>
2024-10-01 20:17:30 +0300

    Update lib.rs add spawn hull

--- src/lib.rs
+++ src/lib.rs
@@ -19,16 +19,16 @@ impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app
             .init_state::<GameState>()
-            .add_plugins(ModulesPlugin);
-            //.add_systems(Startup, test);
+            .add_plugins(ModulesPlugin)
+            .add_systems(Startup, test);
 
 
     }
 }
-/*
+
 fn test(
     mut commands: Commands,
 ) {
     commands.spawn(ModuleBundle::new("simple_hull".to_string(), Transform::from_xyz(0.,0.,0.)));
 }
-*/
+

b0283f2dc74a83cc7f968825b7949910dabd78f1
desolateslacker <91762164+desolateslacker@users.noreply.github.com>
2024-10-04 19:13:13 +0300

    Update grid.rs

--- src/grid.rs
+++ src/grid.rs
@@ -1,18 +1,34 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    color::palettes::css::*,
+};
+use bevy_vector_shapes::prelude::*;
 
 pub struct GridPlugin;
 
 impl Plugin for GridPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(Startup, spawn_grid);
+            .add_systems(Update, draw_grid);
     }
 }
 
-fn spawn_grid(
+#[derive(Component)]
+struct Grid;
+
+fn draw_grid(
+    mut painter: ShapePainter,
+    grid: Query<&Transform, With<Grid>>,
+) {
+    let grid = grid.get_single_mut().expect("err");
+    
+}
+
+pub fn spawn_grid(
     commands: Commands,
 ) {
-    commands.spawn(
-        
-    );
+    commands.spawn((
+        Grid,
+        TransformBundle::default(),
+    ));
 }
